e812ad53260ec9b2dd88029cbefc1cc2174ba5d1
Peal Mazumder <peal6614@gmail.com>
2025-01-24 12:39:51 +0600

    Implement builder design pattern

---
+++ app/src/main/java/com/peal/androidnotebook/designPatterns/builder/DialogBuilder.kt
@@ -0,0 +1,204 @@
+package com.peal.androidnotebook.designPatterns.builder
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.window.Dialog
+
+/**
+ * Created by Peal Mazumder on 23/1/25.
+ */
+
+
+class DialogBuilder {
+    private var title: String = ""
+    private var message: String? = null
+    private var image: Int? = null
+    private var imageWidth: Dp? = null
+    private var imageHeight: Dp? = null
+    private var positiveText: String? = null
+    private var dismissText: String? = null
+
+    private var onPositiveButtonClick: (() -> Unit)? = null
+    private var onNegativeButtonClick: (() -> Unit)? = null
+
+    private var titleTextStyle: TextStyle =
+        TextStyle(color = Color.Black, fontSize = 22.sp, fontWeight = FontWeight.SemiBold)
+    private var messageTextStyle: TextStyle =
+        TextStyle(
+            color = Color.Black.copy(alpha = 0.7f),
+            fontSize = 16.sp,
+            fontWeight = FontWeight.Normal
+        )
+
+    fun setTitle(title: String) = apply { this.title = title }
+    fun setMessage(message: String?) = apply { this.message = message }
+    fun setImage(imageResId: Int) = apply { this.image = imageResId }
+    fun setImageSize(width: Dp?, height: Dp?) = apply {
+        this.imageWidth = width
+        this.imageHeight = height
+    }
+
+    fun setPositiveText(positiveText: String) = apply { this.positiveText = positiveText }
+    fun setNegativeButtonText(dismissText: String) = apply { this.dismissText = dismissText }
+    fun setOnPositiveButtonClick(action: () -> Unit) = apply { this.onPositiveButtonClick = action }
+    fun setOnNegativeButtonClick(action: () -> Unit) = apply { this.onNegativeButtonClick = action }
+    fun setTitleTextStyle(style: TextStyle) = apply { this.titleTextStyle = style }
+    fun setMessageTextStyle(style: TextStyle) = apply { this.messageTextStyle = style }
+
+    @Composable
+    fun BuildDialog(showDialog: Boolean, onDismissRequest: () -> Unit) {
+        if (showDialog) {
+            Dialog(onDismissRequest = { onDismissRequest() }) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(16.dp),
+                    shape = RoundedCornerShape(16.dp),
+                    elevation = CardDefaults.cardElevation(8.dp),
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .fillMaxWidth(),
+                        verticalArrangement = Arrangement.SpaceBetween,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(
+                            text = title,
+                            style = titleTextStyle,
+                            modifier = Modifier
+                                .padding(bottom = 8.dp),
+                            textAlign = TextAlign.Center
+                        )
+
+                        image?.let {
+
+                            Spacer(modifier = Modifier.height(20.dp))
+
+                            Image(
+                                painter = painterResource(id = it),
+                                contentDescription = null,
+                                modifier = Modifier
+                                    .align(Alignment.CenterHorizontally)
+                                    .then(if (imageWidth != null) Modifier.width(imageWidth!!) else Modifier.wrapContentWidth())
+                                    .then(if (imageHeight != null) Modifier.height(imageHeight!!) else Modifier.wrapContentHeight())
+                                    .padding(bottom = 8.dp)
+                            )
+                        }
+
+                        message?.let {
+
+                            Spacer(modifier = Modifier.height(20.dp))
+
+                            Text(
+                                text = it,
+                                style = messageTextStyle,
+                                modifier = Modifier.padding(bottom = 16.dp),
+                                textAlign = TextAlign.Center
+                            )
+                        }
+
+                        Spacer(modifier = Modifier.height(20.dp))
+
+                        Row(
+                            modifier = Modifier.fillMaxWidth()
+                        ) {
+                            positiveText?.let { text ->
+                                GradientButton(
+                                    modifier = Modifier.weight(1f),
+                                    text = text,
+                                    gradientColors = listOf(
+                                        Color(0xFFF7F3B2),
+                                        Color(0xFFBB904E),
+                                    ),
+                                    onClick = {
+                                        onPositiveButtonClick?.invoke()
+                                        onDismissRequest()
+                                    }
+                                )
+                            }
+
+                            dismissText?.let { text ->
+                                Spacer(modifier = Modifier.width(8.dp))
+
+                                GradientButton(
+                                    modifier = Modifier.weight(1f),
+                                    text = text,
+                                    gradientColors = listOf(
+                                        Color(0xFFD8D8D8),
+                                        Color(0xFFA8A9AD),
+                                    ),
+                                    onClick = {
+                                        onNegativeButtonClick?.invoke()
+                                        onDismissRequest()
+                                    }
+                                )
+                            }
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun GradientButton(
+    modifier: Modifier,
+    text: String,
+    gradientColors: List<Color>,
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = modifier
+            .then(
+                Modifier
+                    .height(40.dp)
+                    .clip(RoundedCornerShape(13.dp))
+                    .background(Brush.verticalGradient(gradientColors))
+                    .clickable { onClick() },
+            ),
+
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = text,
+            fontSize = 16.sp,
+            color = Color.Black.copy(alpha = 0.7f)
+        )
+    }
+}
+
+
+
+
---
+++ app/src/main/java/com/peal/androidnotebook/designPatterns/builder/screen/AlertDialogBuilderScreen.kt
@@ -0,0 +1,151 @@
+package com.peal.androidnotebook.designPatterns.builder.screen
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.peal.androidnotebook.R
+import com.peal.androidnotebook.designPatterns.builder.DialogBuilder
+
+/**
+ * Created by Peal Mazumder on 23/1/25.
+ */
+
+@Composable
+fun AlertDialogBuilderScreen(modifier: Modifier = Modifier) {
+    var currentDialog by remember { mutableStateOf<DialogBuilder?>(null) }
+
+    val dialog1 = DialogBuilder()
+        .setTitle("Please Smile!")
+        .setImage(R.drawable.smile)
+        .setImageSize(111.dp, 160.dp)
+        .setPositiveText("Okay")
+        .setOnPositiveButtonClick { currentDialog = null }
+
+    val dialog2 = DialogBuilder()
+        .setTitle("Photo matching unsuccessful!")
+        .setTitleTextStyle(
+            TextStyle(
+                color = Color(0xFFE60026),
+                fontSize = 22.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        )
+        .setMessage("Please contact our customer care")
+        .setImage(R.drawable.photo_mismatch)
+        .setImageSize(100.dp, 100.dp)
+        .setPositiveText("Call 1345")
+        .setNegativeButtonText("Cancel")
+        .setOnPositiveButtonClick { currentDialog = null }
+        .setOnNegativeButtonClick { currentDialog = null }
+
+    val dialog3 = DialogBuilder()
+        .setTitle("Alert!")
+        .setTitleTextStyle(
+            TextStyle(
+                color = Color.Black.copy(alpha = 0.7f),
+                fontSize = 22.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        )
+        .setMessage("Do you want to remove this beneficiary account?")
+        .setPositiveText("Delete")
+        .setNegativeButtonText("Cancel")
+        .setOnPositiveButtonClick { currentDialog = null }
+        .setOnNegativeButtonClick { currentDialog = null }
+
+
+    val dialog4 = DialogBuilder()
+        .setTitle("Disable Touch/Face ID?")
+        .setTitleTextStyle(
+            TextStyle(
+                color = Color.Black.copy(alpha = 0.7f),
+                fontSize = 22.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        )
+        .setImage(R.drawable.biometric)
+        .setMessage("By disabling Touch/Face ID, you can login with PIN only.")
+        .setImageSize(100.dp, 120.dp)
+        .setPositiveText("Disable")
+        .setNegativeButtonText("Cancel")
+        .setOnPositiveButtonClick { currentDialog = null }
+        .setOnNegativeButtonClick { currentDialog = null }
+
+    val dialog5 = DialogBuilder()
+        .setTitle("Congratulations!")
+        .setTitleTextStyle(
+            TextStyle(
+                color = Color(0xFF3DAE72),
+                fontSize = 22.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        )
+        .setImage(R.drawable.biometric_success)
+        .setImageSize(100.dp, 120.dp)
+        .setMessage("You have successfully enabled Touch/Face ID.")
+        .setPositiveText("Done")
+        .setOnPositiveButtonClick { currentDialog = null }
+
+    val dialog6 = DialogBuilder()
+        .setTitle("Record not found")
+        .setTitleTextStyle(
+            TextStyle(
+                color = Color.Black.copy(alpha = 0.7f),
+                fontSize = 22.sp,
+                fontWeight = FontWeight.SemiBold
+            )
+        )
+        .setImage(R.drawable.record_not_found)
+        .setImageSize(100.dp, 100.dp)
+        .setPositiveText("Okay")
+        .setOnPositiveButtonClick { currentDialog = null }
+
+
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Top,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { currentDialog = dialog1 }) {
+            Text("Show Dialog 1")
+        }
+
+        Button(onClick = { currentDialog = dialog2 }) {
+            Text("Show Dialog 2")
+        }
+
+        Button(onClick = { currentDialog = dialog3 }) {
+            Text("Show Dialog 3")
+        }
+
+        Button(onClick = { currentDialog = dialog4 }) {
+            Text("Show Dialog 4")
+        }
+
+        Button(onClick = { currentDialog = dialog5 }) {
+            Text("Show Dialog 5")
+        }
+
+        Button(onClick = { currentDialog = dialog6 }) {
+            Text("Show Dialog 6")
+        }
+
+        currentDialog?.BuildDialog(
+            showDialog = currentDialog != null,
+            onDismissRequest = { currentDialog = null }
+        )
+    }
+}
